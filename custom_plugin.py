87142aec300771ea12f8284c2afbb04fcb998a20
Elton Pereira <eltonplima@gmail.com>
2020-03-20 09:42:38 +0000

    Update custom_plugin.py

--- custom_plugin.py
+++ custom_plugin.py
@@ -1,5 +1,6 @@
 def run(job, logger, **kwargs):
     logger.info('Im playing with jobs')
     print('Test if this modification go to the server in "real time"')
+    print(dir(job))
 
     return "SUCCESS", "It's alive!!!", ""
