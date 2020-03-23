2759d7dfd2197a352c4dddd926f5469c7d116396
Elton Pereira <eltonplima@gmail.com>
2020-03-23 16:58:49 +0000

    Update custom_plugin_2.py

--- custom_plugin_2.py
+++ custom_plugin_2.py
@@ -1,6 +1,8 @@
 def run(job, logger, **kwargs):
     logger.info("I'm playing with jobs again")
+    print('{:#^80}'.format('job'))
     print(dir(job))
+    print('{:#^80}'.format('kwargs'))
     print(kwargs)
 
     return "SUCCESS", "It's alive!!!", ""
